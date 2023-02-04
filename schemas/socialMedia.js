e14db953ba6d68138bab1fe24465304edc774135
Kunal Kathpal <kkunal2103@outlook.com>
2023-02-04 21:31:22 +0530

    Add files via upload

---
+++ schemas/socialMedia.js
@@ -0,0 +1,16 @@
+export default {
+    name:'socialMedia',
+    title:'Social Media',
+    type:'document',
+    fields:[
+           {    name:'name',
+                title:'Name',
+                type:'string'
+            },
+            {
+                name:'link',
+                title:'Link',
+                type:'string'
+            }
+    ]
+}
